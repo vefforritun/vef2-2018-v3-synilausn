18ac1ad05107d6e73a4ad8db0a972f119a169255
Ólafur Sverrir Kjartansson <kjarni@gmail.com>
2018-03-27 17:34:33 +0000

    Bæta við CORS headerum

--- app.js
+++ app.js
@@ -5,6 +5,14 @@ const api = require('./api');
 const app = express();
 
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  next();
+});
+
 app.use('/', api);
 
 function notFoundHandler(req, res, next) { // eslint-disable-line
